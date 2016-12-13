eb4763f529dd52f7cf12fda7f7624f0493ad3b39
ZachCutler <zachary.cutler@drake.edu>
2016-12-13 16:51:12 -0600

    Initial commits for Messaging. Null error on OnClickListener in the fragment2.

--- UnityPoint/app/src/main/java/com/example/kristinaneel/unitypoint/TabFragment1.java
+++ UnityPoint/app/src/main/java/com/example/kristinaneel/unitypoint/TabFragment1.java
@@ -1,19 +1,45 @@
 package com.example.kristinaneel.unitypoint;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Troy on 11/25/2016.
  */
 
 public class TabFragment1 extends Fragment {
+    private Button myButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+        View rootView = inflater.inflate(R.layout.tab_fragment_2, container, false);
+        try {
+            List<Messages> messageList = new JsonReaderMessage((MainActivity) getActivity()).getPersonList();
+            Messages[] messages = messageList.toArray(new Messages[messageList.size()]);
+            ArrayAdapter<Messages> adapter = new ArrayAdapter<Messages>
+                    ((MainActivity) getActivity(), android.R.layout.simple_list_item_1, messages);
+            myButton = (Button) rootView.findViewById(R.id.button2);
+
+            myButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent((MainActivity) getActivity(), MessageActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return rootView;
     }
 }
